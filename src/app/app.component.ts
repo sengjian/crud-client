b1cd32ea55c80023917df474df4327689ac40c2d
SengJian <sseng@nextlabs.com>
2023-11-15 23:19:30 +0800

    Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,106 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
+import { v1 as uuid } from 'uuid';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    <router-outlet></router-outlet>
-  `,
-  styles: []
+  templateUrl: './app.component.html',
+  styles: [],
+  providers: []
 })
 export class AppComponent {
-  title = 'crud-client';
+  // title = 'crud-client';
+  customers: any[] = [];
+  titles: any = ['MR', 'MRS', 'SIR', 'TUN', 'CIK', 'ENCIK'];
+  customerDetailForm = this.fb.group({
+    id: null,
+    name: ['', [Validators.required, Validators.maxLength(45)]],
+    title: [''],
+    ic: [
+      '',
+      [
+        Validators.maxLength(14),
+        Validators.required,
+        Validators.pattern(/^\d{6}-\d{2}-\d{4}$/gi),
+      ],
+    ],
+  });
+
+  constructor(private fb: FormBuilder, private http: HttpClient) { }
+
+  ngOnInit(): void {
+    const url: string = '/assets/customers.json';
+    this.http.get(url)
+      .pipe(
+        map(
+          (jsonData: any) => (
+            {
+              data: jsonData
+            }
+          )
+        )
+      ).subscribe((response) => {
+        this.customers = response.data;
+      });
+  }
+
+  updateProfile() {
+    this.customerDetailForm.patchValue({});
+  }
+
+  changeTitle(e: any) {
+    console.log(e.value);
+    this.title?.setValue(e.target.value, {
+      onlySelf: true,
+    });
+  }
+
+  get name() {
+    return this.customerDetailForm.get('name');
+  }
+
+  get ic() {
+    return this.customerDetailForm.get('ic');
+  }
+
+  get title() {
+    return this.customerDetailForm.get('title');
+  }
+
+  get dob() {
+    return this.customerDetailForm.get('ic')?.valid ? (
+      this.customerDetailForm.get('ic')?.value?.substring(4, 6) +
+      '/' +
+      this.customerDetailForm.get('ic')?.value?.substring(2, 4) +
+      '/' +
+      this.customerDetailForm.get('ic')?.value?.substring(0, 2)
+    ) : '';
+  }
+
+  editCustomer(item: any) {
+    this.clearForm()
+    this.customerDetailForm.patchValue(item);
+    console.log(this.customerDetailForm.value)
+  }
+
+  deleteCustomer(item: any) {
+    let index = this.customers.findIndex(val => val.id == item.id)
+    if (index > -1) this.customers.splice(index, 1)
+  }
+
+  clearForm(){
+    this.customerDetailForm.reset();
+  }
+
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.customerDetailForm.value);
+    if(this.customerDetailForm.value.id) {
+      let index = this.customers.findIndex(val => val.id == this.customerDetailForm.value.id)
+      if (index > -1) this.customers[index] = this.customerDetailForm.value
+    }
+    else this.customers.push({ id: uuid(), ...this.customerDetailForm.value });
+  }
 }
